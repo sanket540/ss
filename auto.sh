c57d0d0fd3260995093b233416c72705288b833e
sanket540 <71389116+sanket540@users.noreply.github.com>
2022-07-24 23:36:10 +0530

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -5,7 +5,7 @@ cd lab_exp
 wget https://raw.githubusercontent.com/sanket540/ss/main/lab.zip --no-check-certificate
 unzip lab.zip
 mkdir exp
-mv $1 /exp
+mv $1/ exp/
 rm -rf pg*
 rm -rf lab.zip
 cd ..
