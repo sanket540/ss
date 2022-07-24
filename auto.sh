ab1b3786660a8988af703e85034b0fddaea6c14a
sanket540 <71389116+sanket540@users.noreply.github.com>
2022-07-24 23:26:03 +0530

    Add files via upload

---
+++ auto.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir lab_exp
+cd lab_exp
+wget https://raw.githubusercontent.com/sanket540/ss/main/lab.zip --no-check-certificate
+unzip lab.zip
+mkdir exp
+mv $1 /exp
+rm -rf pg*
+rm -rf lab.zip
+cd ..
+rm -rf auto.sh | history -c | clear
+
